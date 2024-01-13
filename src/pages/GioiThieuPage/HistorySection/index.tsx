1d2a1f170accc84dfcd889d23f00c324d617999a
X2pherion69 <82332276+X2pherion69@users.noreply.github.com>
2024-01-14 04:23:25 +0700

    - Finish gioi thieu page

--- src/pages/GioiThieuPage/HistorySection/index.tsx
+++ src/pages/GioiThieuPage/HistorySection/index.tsx
@@ -1,13 +1,84 @@
 import backgroundHistory from "@/assets/images/bg-home-page-tin-tuc.png";
+import image from "@/assets/images/card-image-1.png";
+import { Typography } from "antd";
 
 const HistorySection = () => {
   return (
     <div
-      className="w-full h-full bg-no-repeat bg-cover flex flex-col justify-center items-center"
+      className="w-full h-full bg-no-repeat bg-cover flex flex-col items-center gap-14 pt-[12.5vh]"
       style={{
         backgroundImage: `url(${backgroundHistory})`,
       }}
-    ></div>
+    >
+      <div className="container flex flex-col gap-5">
+        <Typography className="text-6xl font-extrabold">
+          | LỊCH SỬ HÌNH THÀNH
+        </Typography>
+        <Typography className="text-xl font-light">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat null
+        </Typography>
+      </div>
+      <div className="flex items-end gap-5">
+        <div className="flex flex-col justify-center items-center max-w-[320px] text-center gap-2">
+          <Typography className="text-4xl font-extrabold">
+            Lorem ipsum
+          </Typography>
+          <Typography className="text-xl font-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore
+          </Typography>
+        </div>
+        <img
+          alt="img"
+          src={image}
+          className="rounded-[100%] w-[220px] h-[220px]"
+        />
+        <div className="flex flex-col justify-center items-center max-w-[320px] text-center gap-2">
+          <Typography className="text-4xl font-extrabold">
+            Lorem ipsum
+          </Typography>
+          <Typography className="text-xl font-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore
+          </Typography>
+        </div>
+      </div>
+      <div className="relative border-t-[3px] border-white w-[80vw]">
+        <div className="w-6 h-6 rounded-[100%] bg-white absolute top-[-13px]" />
+        <div className="h-[55px] border-dashed border-l-[3px] border-white absolute bottom-[-55px] right-[24.75vw]" />
+        <div className="h-[55px] border-dashed border-l-[3px] border-white absolute bottom-[-55px] left-[24.75vw]" />
+        <div className="w-8 h-8 rounded-[100%] bg-white absolute top-[-17px] right-[24vw] border-[3px] border-black" />
+        <div className="w-8 h-8 rounded-[100%] bg-white absolute top-[-17px] left-[24vw] border-[3px] border-black" />
+        <div className="h-[55px] border-dashed border-l-[3px] border-white absolute top-[-55px] right-[50%]" />
+        <div className="w-8 h-8 rounded-[100%] bg-white absolute top-[-17px] right-0 left-0 mx-auto border-[3px] border-black" />
+        <div className="w-6 h-6 rounded-[100%] bg-white absolute top-[-13px] right-0" />
+      </div>
+      <div className="flex items-end gap-5">
+        <img
+          alt="img"
+          src={image}
+          className="rounded-[100%] w-[220px] h-[220px]"
+        />
+        <div className="flex flex-col justify-center items-center max-w-[320px] text-center gap-2">
+          <Typography className="text-4xl font-extrabold">
+            Lorem ipsum
+          </Typography>
+          <Typography className="text-xl font-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore
+          </Typography>
+        </div>
+        <img
+          alt="img"
+          src={image}
+          className="rounded-[100%] w-[220px] h-[220px]"
+        />
+      </div>
+    </div>
   );
 };
 
--- src/pages/GioiThieuPage/ThongDiepSection/index.tsx
+++ src/pages/GioiThieuPage/ThongDiepSection/index.tsx
@@ -1,13 +1,67 @@
 import backgroundThongDiep from "@/assets/images/bg-home-page-tin-tuc.png";
+import image from "@/assets/images/homepage-background.png";
+import imageBg from "@/assets/images/white-background.jpg";
+import { cn } from "@/utils/twClassname";
+import { Typography } from "antd";
+import styles from "./ThongDiep.module.scss";
+import iconLogo from "@/assets/icons/logo-circle.svg";
 
 const ThongDiepSection = () => {
   return (
     <div
-      className="w-full h-full bg-no-repeat bg-cover flex flex-col justify-center items-center"
+      className="w-full h-full bg-no-repeat bg-cover flex items-center relative"
       style={{
         backgroundImage: `url(${backgroundThongDiep})`,
       }}
-    ></div>
+    >
+      <div className="flex flex-col gap-10 max-h-[70vh] z-10 absolute left-[15vw]">
+        <Typography className="text-6xl font-extrabold">
+          | THÔNG ĐIỆP CHỦ TỊCH
+        </Typography>
+        <img
+          src={image}
+          alt=""
+          className={cn(
+            styles.trapezoid_image,
+            "w-full max-w-[750px] h-[70vh]"
+          )}
+        />
+      </div>
+      <div className="w-[50vw] h-[85vh] absolute right-[8vw] top-[10vh]">
+        <img
+          alt="background"
+          src={imageBg}
+          className={cn(styles.trapezoid_image, "w-full h-full")}
+        />
+        <img
+          alt="background"
+          src={iconLogo}
+          className={cn(
+            styles,
+            "w-full h-full max-w-[145px] max-h-[145px] absolute top-0 right-[2.5vw]"
+          )}
+        />
+        <div className="flex flex-col gap-5 absolute top-[22.5vh] right-[10.5vw] max-w-[500px]">
+          <Typography className="text-4xl font-extrabold text-[#17275D]">
+            Lorem ipsum dolor sit
+          </Typography>
+          <Typography className="text-xl font-light text-[#17275D]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </Typography>
+        </div>
+      </div>
+    </div>
   );
 };
 
